7cd743d8de83909cc3e5c06056b7ea9f69486a55
jevin2016 <jevinpatel1996@gmail.com>
2024-03-16 19:07:32 +0530

    login Screen

---
+++ Gallery App/View/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Gallery App
+//
+//  Created by jevin kamani on 16/03/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func loginBtnTouch(_ sender: UIButton) {
+        
+    }
+    
+}
